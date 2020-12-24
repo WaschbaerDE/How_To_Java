8ebdd61265891f92ee5d0ce415587b2e533bcf93
Martin <Martin@DESKTOP-EVN7HID>
2020-12-24 22:03:56 +0100

    Changing Readme.md

--- src/Hello_World/Main.java
+++ src/Hello_World/Main.java
@@ -1,10 +1,7 @@
-package com.company;
+package Hello_World;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Frohe Weihnachten!");
-
     }
 }
